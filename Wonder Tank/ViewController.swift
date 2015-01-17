3db051215b150b06d214d8dce25d10d29bf82ae0
ZachPurdue <perryz@purdue.edu>
2015-01-17 03:44:34 -0500

    Initial UI code

---
+++ Wonder Tank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wonder Tank
+//
+//  Created by Zach Perry on 1/17/15.
+//  Copyright (c) 2015 Zach Perry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
